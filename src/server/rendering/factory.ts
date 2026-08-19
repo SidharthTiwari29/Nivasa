a6f3a698966903bdb746362fe8ef40d09f075a7f
SidharthTiwari29 <Sidharth2906@gmail.com>
2026-08-19 19:25:53 +0530

    feat: enforce provider-neutral rendering configuration boundary

---
+++ src/server/rendering/factory.ts
@@ -0,0 +1,9 @@
+import type { RenderProvider } from './provider';
+
+export class RenderProviderNotConfiguredError extends Error {
+  readonly code = 'NOT_CONFIGURED';
+}
+
+export function getRenderProvider(): RenderProvider {
+  throw new RenderProviderNotConfiguredError('RENDER_PROVIDER: NOT_CONFIGURED; configure a concrete renderer before creating render jobs');
+}
